f100d298a3d766bf892d071a92c7eed01d7eeb30
Abigail Hocking <nin@nkli.net>
2020-02-23 00:04:34 +0000

    Correct erroneous and important for documentation comment

--- Scripts/NKLI.Nigiri.SVO.cs
+++ Scripts/NKLI.Nigiri.SVO.cs
@@ -46,18 +46,14 @@ namespace NKLI.Nigiri.SVO
                 PackStruct(bitFieldOccupancy, runLength, depth, isRoot);
             }
 
-            // Pack 64 bits
+            // Pack 32 bits
             // BO = Bitfield occupancy, 8b
             // RL = Sparse runlength, 4b
             // OD = Octree depth of this node, 4b
             // IR = Is this a root node, 1b
             // Structure [00] [01] [02] [03] [04] [05] [06] [07] [08] [09] [10] [11] [12] [13] [14] [15]
-            //            MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC
-            //           [16] [17] [18] [19] [20] [21] [22] [23] [24] [25] [26] [27] [28] [29] [30] [31]
-            //            MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC   MC
-            // Structure [32] [33] [34] [35] [36] [37] [38] [39] [40] [41] [42] [43] [44] [45] [46] [47]
             //            BO   BO   BO   BO   BO   BO   BO   BO   RL   RL   RL   RL   OD   OD   OD   OD
-            //           [48] [49] [50] [51] [52] [53] [54] [55] [56] [57] [58] [59] [60] [61] [62] [63]
+            //           [16] [17] [18] [19] [20] [21] [22] [23] [24] [25] [26] [27] [28] [29] [30] [31]
             //            IR   --   --   --   --   --   --   --   --   --   --   --   --   --   --   --
             public void PackStruct(uint bitFieldOccupancy, uint runLength, uint depth, bool isRoot)
             {
