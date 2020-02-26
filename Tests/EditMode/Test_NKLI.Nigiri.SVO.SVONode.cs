b38d2e193db137917aa78e86e28346a725315d75
Abigail Hocking <nin@nkli.net>
2020-02-26 20:31:37 +0000

    Breakout SVONode test units to separate file.

---
+++ Tests/EditMode/Test_NKLI.Nigiri.SVO.SVONode.cs
@@ -0,0 +1,114 @@
+﻿/// <summary>
+/// NKLI     : Nigiri - Test Unit - SVONode
+/// Copywrite: Abigail Sara Hocking of Newbury, 2020. 
+/// Licence  : The Nigiri 'Bits and pieces' Licence. [v3]
+/// </summary>
+
+using NKLI.Nigiri.SVO;
+using NUnit.Framework;
+
+namespace Tests.Nigiri.SVO
+{
+    public class Test_SVONode
+    {
+        // Test PackStruct bitfield occupancy
+        [Test]
+        public void PackStruct_BitfieldOccupancy()
+        {
+            uint control = 0b_1111_1111_0000_0000_0000_0000_0000_0000;
+            uint sample = 0b_0000_0000_0000_0000_0000_0000_1111_1111;
+
+            SVONode SVONode = new SVONode(0, sample, 0, 0, false);
+            //Debug.Log($"<Unit Test> (PackStruct_BitfieldOccupancy) Control:  {Convert.ToString(control, toBase: 2)}");
+            //Debug.Log($"<Unit Test> (PackStruct_BitfieldOccupancy) Sample:  {Convert.ToString(SVONode.packedBitfield, toBase: 2)}");
+
+            Assert.AreEqual(SVONode.packedBitfield, control);
+        }
+
+        // Test PackStruct run length
+        [Test]
+        public void PackStruct_RunLength()
+        {
+            uint control = 0b_0000_0000_1111_0000_0000_0000_0000_0000;
+            uint sample = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
+
+            SVONode SVONode = new SVONode(0, 0, sample, 0, false);
+
+            Assert.AreEqual(SVONode.packedBitfield, control);
+        }
+
+        // Test PackStruct octree depth
+        [Test]
+        public void PackStruct_OctreeDepth()
+        {
+            uint control = 0b_0000_0000_0000_1111_0000_0000_0000_0000;
+            uint sample = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
+
+            SVONode SVONode = new SVONode(0, 0, 0, sample, false);
+
+            Assert.AreEqual(SVONode.packedBitfield, control);
+        }
+
+        // Test PackStruct is leaf
+        [Test]
+        public void PackStruct_IsLeaf()
+        {
+            uint control = 0b_0000_0000_0000_0000_1000_0000_0000_0000;
+
+            SVONode SVONode = new SVONode(0, 0, 0, 0, true);
+
+            Assert.AreEqual(SVONode.packedBitfield, control);
+        }
+
+        // Test UnPackStruct bitfield occupancy
+        [Test]
+        public void UnPackStruct_BitfieldOccupancy()
+        {
+            uint control = 0b_0000_0000_0000_0000_0000_0000_1111_1111;
+            uint sample = 0b_1111_1111_0000_0000_0000_0000_0000_0000;
+
+            SVONode SVONode = new SVONode(0, sample);
+            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
+
+            Assert.AreEqual(_bitFieldOccupancy, control);
+        }
+
+        // Test UnPackStruct run length
+        [Test]
+        public void UnPackStruct_RunLength()
+        {
+            uint control = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
+            uint sample = 0b_0000_0000_1111_0000_0000_0000_0000_0000;
+
+            SVONode SVONode = new SVONode(0, sample);
+            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
+
+            Assert.AreEqual(_runLength, control);
+        }
+
+        // Test UnPackStruct octree depth
+        [Test]
+        public void UnPackStruct_OctreeDepth()
+        {
+            uint control = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
+            uint sample = 0b_0000_0000_0000_1111_0000_0000_0000_0000;
+
+            SVONode SVONode = new SVONode(0, sample);
+            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
+
+            Assert.AreEqual(_depth, control);
+        }
+
+        // Test UnPackStruct is leaf
+        [Test]
+        public void UnPackStruct_IsLeaf()
+        {
+            uint sample = 0b_0000_0000_0000_0000_1000_0000_0000_0000;
+
+            SVONode SVONode = new SVONode(0, sample);
+            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
+
+            Assert.AreEqual(isLeaf, true);
+        }
+    }
+}
--- Tests/EditMode/Test_NKLI.Nigiri.SVO.cs
+++ Tests/EditMode/Test_NKLI.Nigiri.SVO.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/// <summary>
+/// NKLI     : Nigiri - Test Unit SVO
+/// Copywrite: Abigail Sara Hocking of Newbury, 2020. 
+/// Licence  : The Nigiri 'Bits and pieces' Licence. [v3]
+/// </summary>
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -405,112 +411,6 @@ namespace Tests.Nigiri.SVO
             // Validate buffers are the same length.
             // This also ensures that the count does not exceed the length of either buffer.  
             return b1.Length == b2.Length && memcmp(b1, b2, b1.Length) == 0;
-        }
-    }
-    #endregion
-
-
-    #region Test_SVONode
-    public class Test_SVONode
-    {
-        // Test PackStruct bitfield occupancy
-        [Test]
-        public void PackStruct_BitfieldOccupancy()
-        {
-            uint control = 0b_1111_1111_0000_0000_0000_0000_0000_0000;
-            uint sample  = 0b_0000_0000_0000_0000_0000_0000_1111_1111;
-
-            SVONode SVONode = new SVONode(0, sample, 0, 0, false);
-            //Debug.Log($"<Unit Test> (PackStruct_BitfieldOccupancy) Control:  {Convert.ToString(control, toBase: 2)}");
-            //Debug.Log($"<Unit Test> (PackStruct_BitfieldOccupancy) Sample:  {Convert.ToString(SVONode.packedBitfield, toBase: 2)}");
-
-            Assert.AreEqual(SVONode.packedBitfield, control);
-        }
-
-        // Test PackStruct run length
-        [Test]
-        public void PackStruct_RunLength()
-        {
-            uint control = 0b_0000_0000_1111_0000_0000_0000_0000_0000;
-            uint sample = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
-
-            SVONode SVONode = new SVONode(0, 0, sample, 0, false);
-
-            Assert.AreEqual(SVONode.packedBitfield, control);
-        }
-
-        // Test PackStruct octree depth
-        [Test]
-        public void PackStruct_OctreeDepth()
-        {
-            uint control = 0b_0000_0000_0000_1111_0000_0000_0000_0000;
-            uint sample = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
-
-            SVONode SVONode = new SVONode(0, 0, 0, sample, false);
-
-            Assert.AreEqual(SVONode.packedBitfield, control);
-        }
-
-        // Test PackStruct is leaf
-        [Test]
-        public void PackStruct_IsLeaf()
-        {
-            uint control = 0b_0000_0000_0000_0000_1000_0000_0000_0000;
-
-            SVONode SVONode = new SVONode(0, 0, 0, 0, true);
-
-            Assert.AreEqual(SVONode.packedBitfield, control);
-        }
-
-        // Test UnPackStruct bitfield occupancy
-        [Test]
-        public void UnPackStruct_BitfieldOccupancy()
-        {
-            uint control = 0b_0000_0000_0000_0000_0000_0000_1111_1111;
-            uint sample = 0b_1111_1111_0000_0000_0000_0000_0000_0000;
-
-            SVONode SVONode = new SVONode(0, sample);
-            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
-
-            Assert.AreEqual(_bitFieldOccupancy, control);
-        }
-
-        // Test UnPackStruct run length
-        [Test]
-        public void UnPackStruct_RunLength()
-        {
-            uint control = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
-            uint sample = 0b_0000_0000_1111_0000_0000_0000_0000_0000;
-
-            SVONode SVONode = new SVONode(0, sample);
-            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
-
-            Assert.AreEqual(_runLength, control);
-        }
-
-        // Test UnPackStruct octree depth
-        [Test]
-        public void UnPackStruct_OctreeDepth()
-        {
-            uint control = 0b_0000_0000_0000_0000_0000_0000_0000_1111;
-            uint sample = 0b_0000_0000_0000_1111_0000_0000_0000_0000;
-
-            SVONode SVONode = new SVONode(0, sample);
-            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
-
-            Assert.AreEqual(_depth, control);
-        }
-
-        // Test UnPackStruct is leaf
-        [Test]
-        public void UnPackStruct_IsLeaf()
-        {
-            uint sample = 0b_0000_0000_0000_0000_1000_0000_0000_0000;
-
-            SVONode SVONode = new SVONode(0, sample);
-            SVONode.UnPackStruct(out uint _bitFieldOccupancy, out uint _runLength, out uint _depth, out bool isLeaf);
-
-            Assert.AreEqual(isLeaf, true);
         }
     }
     #endregion
